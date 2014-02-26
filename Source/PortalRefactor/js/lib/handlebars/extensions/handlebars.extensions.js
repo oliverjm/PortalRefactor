6479bb7975278e7088f392580ad739b96b53b9b1
Josh Oliver <joliver111222@gmail.com>
2014-02-26 18:18:19 -0500

    Add libraries - bootstrap, jquery, handlebars Add css - bootstrap, application, site Update index.html with header, footer, leftnav, and content Start correctly using require.js

---
+++ Source/PortalRefactor/js/lib/handlebars/extensions/handlebars.extensions.js
@@ -0,0 +1,15 @@
+﻿Handlebars.getTemplate = function (name) {
+    if (Handlebars.templates === undefined || Handlebars.templates[name] === undefined) {
+        $.ajax({
+            url: './' + name + '.handlebars',
+            success: function (data) {
+                if (Handlebars.templates === undefined) {
+                    Handlebars.templates = {};
+                }
+                Handlebars.templates[name] = Handlebars.compile(data);
+            },
+            async: false
+        });
+    }
+    return Handlebars.templates[name];
+};
---
+++ Source/PortalRefactor/js/main.js
@@ -0,0 +1,24 @@
+﻿requirejs.config({
+    baseUrl: 'js/lib',
+    paths: {
+        'jquery':'jquery/jquery',
+        'bootstrap': 'bootstrap/bootstrap.min',
+        'handlebars': 'handlebars/handlebars-v1.3.0',
+        'handlebarsExtensions': 'handlebars/extensions/handlebars.extensions'
+    },
+    shim: {
+        'bootstrap': {
+            deps: ['jquery']
+        },
+        'handlebars': {
+            exports: 'Handlebars'
+        },
+        'handlebarsExtensions': {
+            deps: ['handlebars']
+        }
+    }
+});
+
+requirejs(['jquery', 'handlebars', 'handlebarsExtensions'], function ($, Handlebars) {
+    $('#app').html('Hello World');
+});
